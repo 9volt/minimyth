bc82c00c35b6915ee661fcc762719b4da2e7295f
Chrysothemis <sonya@utecht.co>
2017-04-22 14:54:44 -0500

    new files

---
+++ src/components/Unit.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Unit.css';
+
+class Unit extends Component {
+  render() {
+    return (
+        <span>Unit </span>
+    );
+  }
+}
+
+export default Unit;
